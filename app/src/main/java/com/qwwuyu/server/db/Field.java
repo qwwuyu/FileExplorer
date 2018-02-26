9ae320f8b36b1fbe55967a7f50648fb9801467c4
qwwuyu <qiweiwuyu@gmail.com>
2018-02-26 18:33:31 +0800

    删除无用代码

--- app/src/main/java/com/qwwuyu/server/db/Field.java
+++
@@ -1,7 +0,0 @@
-package com.qwwuyu.server.db;
-
-public class Field {
-    public static final String ctrl_tb = "ctrl_tb";
-    public static final String id = "id";
-    public static final String name = "name";
-}
